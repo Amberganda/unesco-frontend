7d625abb25f31abfe93a3604f0e16b73f0a28bdb
Amber Taranto <ambzzz@gmail.com>
2020-08-16 14:45:58 -0700

    installed packages and imported packages to index.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from "react";
 
 import "./App.css";
 class App extends Component {
-  componentDidMount() {
-    fetch("http://localhost:3000/locations", {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
+  // componentDidMount() {
+  //   fetch("http://localhost:3000/locations", {
+  //     method: "GET",
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }
 
   render() {
     return <div className="App">App</div>;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { createStore, applyMiddleware } from "redux"; //comes directly from Redux library //need middleware too, comes from redux.
+import thunk from "redux-thunk";
+import { Provider } from "react-redux"; //wrap apps so we can use redux. any component that we wrap in provider will have access to our redux store. will wrap our app in there.
+
+//set up store
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
