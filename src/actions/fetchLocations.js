cce329836cb9ee24c4c7525cb0d45ad50d068603
Amber Taranto <ambzzz@gmail.com>
2020-08-16 19:09:22 -0700

    implemented fetchLocations action with redux

--- src/actions/fetchLocations.js
+++ src/actions/fetchLocations.js
@@ -2,11 +2,12 @@
 //what we return from this function for what we dispatch is an action object (or just an action)
 // so an action creator creates an action object, and then that action object will be dispatched to our reducer
 //which will then return our new version of our state based on the action that we sent.
-export function fetchLocations(action) {
-  //   fetch("http://localhost:3000/locations")
-  //     .then((response) => response.json())
-  //     .then((data) => console.log(data));
-  //THUNK LETS US CALL DISPATCH FROM INSIDE THIS ACTION CREATOR.
-  //   dispatch();
-  return action;
+export function fetchLocations() {
+  return (dispatch) => {
+    fetch("http://localhost:3000/locations")
+      .then((response) => response.json())
+      .then((data) => dispatch({ type: "GET_LOCATIONS_SUCCESS", data: data }));
+    //THUNK LETS US CALL DISPATCH FROM INSIDE THIS ACTION CREATOR.
+    //   dispatch();
+  };
 }
